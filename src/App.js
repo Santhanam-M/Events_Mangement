8f8180d70fd4be9edadcd30e85f3c2309980b23c
santhanam <santhanamui@gmail.com>
2024-05-12 21:05:06 +0530

    comment component created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Comment from './Components/Comment';
+
 const App = () =>{
 
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(2),
+    color: theme.palette.text.secondary,
+    borderRadius : theme.spacing(2)
+  }));
+  
+  return (
+          <Grid container spacing={2} justifyContent="center">
+            <Grid item xs={3}>
+                <Item>
+                  <Comment/>
+                </Item>
+            </Grid>
+          </Grid>)
 
 }
 
---
+++ src/Components/Comment.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import IconButton from "@mui/material/IconButton";
+import SendIcon from "@mui/icons-material/Send";
+
+const Comment = () => {
+  const [comments, setComments] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      id: Number(new Date()),
+      comments: comments,
+    };
+  };
+
+  return (
+    <div>
+      <h4>Comments</h4>
+
+      <form onSubmit={handleSubmit}>
+        <TextField
+          fullWidth
+          id="outlined-multiline-flexible"
+          label="Write Comment"
+          multiline
+          size="small"
+          value={comments}
+          onChange={(e) => {
+            setComments(e.target.value);
+          }}
+          InputProps={{
+            endAdornment: (
+              <IconButton
+                type="submit"
+                color="secondary"
+                aria-label="send"
+                size="small"
+              >
+                <SendIcon />
+              </IconButton>
+            ),
+          }}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Comment;
